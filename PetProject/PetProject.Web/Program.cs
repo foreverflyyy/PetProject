d077edf86e3a20f4b8786d6715f1c0a7fd908f8c
Nikita <nikita.ushakov.02@outlook.com>
2023-04-13 18:12:22 +0200

    dev: returned files for spa proxy + added comments

--- PetProject/PetProject.Web/Program.cs
+++ PetProject/PetProject.Web/Program.cs
@@ -11,12 +11,15 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Позволяет обслуживать файлы
 app.UseStaticFiles();
 app.UseRouting();
 
 
 app.MapControllers();
 
+// Включает обслуживание документа по умолчанию для любого неизвестного запроса, получаемого сервером
 app.MapFallbackToFile("index.html"); ;
 
 app.Run();
